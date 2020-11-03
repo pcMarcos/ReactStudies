f2645034b1d3ecb8d349e818982b91ed96363eaf
pcMarcos <mp.doublebass@gmail.com>
2020-11-02 22:55:39 -0300

    Create new React projects

---
+++ keeper-app-part-1-starting/src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function Note(){
+    return(<div className="note">
+        <h1 className="h1"> This is the card title</h1>
+        <p className="p"> This is the card content</p>
+    </div>);
+}
+
+export default Note;
